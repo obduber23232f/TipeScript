0aef07a972cb032537c6ca0ad7b17237be7e0e8f
obduber <obduber901@@gmail.com>
2020-03-05 13:13:51 -0400

    clase de 05/03/20

--- clase1/main.js
+++ clase1/main.js
@@ -8,7 +8,17 @@ casado = false;
 console.log(nombre);
 console.log(f);
 console.log(casado);
-function saludar(nombre) {
-    alert("hola" + nombre);
+/* function saludar(nombre:string){
+alert("hola" +nombre)
 }
-saludar("obduber");
+saludar("obduber")}
+let nombre:string = prompt ("Escribe tu nombre")
+*/
+function sumar(f, n2) {
+    return f;
+}
+sumar(9, 200);
+console.log(sumar);
+function promedia(n1, n2, n3, solo_sumar) {
+    var suma = ;
+}
--- clase1/main.ts
+++ clase1/main.ts
@@ -11,7 +11,28 @@ console.log(nombre)
 console.log(f)
 console.log(casado)
 
-function saludar(nombre:string){
+/* function saludar(nombre:string){
 alert("hola" +nombre)
 }
-saludar("obduber")
+saludar("obduber")}
+let nombre:string = prompt ("Escribe tu nombre")
+*/
+
+/* function sumar(f:number,n2:number):number{
+    return f
+}
+sumar(9,200)
+console.log(sumar)
+ */
+function promedia(n1:number,n2:number,n3:number, solo_sumar?:boolean){
+
+    let suma:number = n1+n2+n3
+    if(solo_sumar){
+        return suma 
+    }else{
+        return suma/3
+    }
+    
+}
+promedia(10,20,30)
+console.log(promedia)
