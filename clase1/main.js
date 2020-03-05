64a25c49fe6198243e2192b68c53ff82a9dff946
obduber <obduber901@@gmail.com>
2020-03-05 12:15:13 -0400

    clase de 05/03/20

--- clase1/main.js
+++ clase1/main.js
@@ -1,2 +1,7 @@
-var nombre = "obduber";
-alert("ooo" + nombre);
+var nombre;
+var apellido;
+var f;
+var casado;
+nombre = "obduber";
+f = 18;
+casado = false;
--- clase1/main.ts
+++ clase1/main.ts
@@ -1,2 +1,9 @@
-let nombre = "obduber";
-alert("ooo" +nombre);
+let nombre:string
+let apellido:string
+let f:number
+let casado:boolean
+
+nombre = "obduber"
+f = 18
+casado = false
+
